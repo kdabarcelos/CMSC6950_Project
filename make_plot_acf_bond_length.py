ed44d750a60e6af7886163857f066ef681efe602
Karina Barcelos <kdabarcelos@mun.ca>
2021-06-17 09:11:09 -0230

    adding comments

--- make_plot_acf_bond_length.py
+++ make_plot_acf_bond_length.py
@@ -1,26 +1,29 @@
 #Task 1 - example 1: analyze the CO bond stretching motion using its time autocorrelation function (acf) from its time series of a MD simulation.
 
 #About this script
-        #It creates one 2x2 plot with length versus time of C=O and C-H bond lenths, and their autocorrelation funtion (acf)
+        #It creates one 2x2 plot with length versus time of C=O and C-H bond lengths, and their autocorrelation funtion (acf)
 	#It is necessary to run create_acf.py first before plotting to obtain the intermediate files with acf values of C=O and C-H bond lengths
 
 #Run command example [python3 script.py input_file1.txt input_file2.txt output_figure_name.png]
         #$python3 make_plot_acf.py CO_acf.txt CH_acf.txt CO_CH_lengths_acf.png
 
+#importing modules used
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
+#assigning arguments into input1/input2/output for the command-line
 args = sys.argv[1:]
 input1 = args[0]
 input2 = args[1]
 output = args[2]
 
+#defining the main funtion for plotting figure
 def main(input1, input2, output):
     '''
     Read text files from create_acf.py and plot 2x2 length (and its average) versus time, and acf versus lag
     '''
-    
+    #reading/importing input file
     dfCH = pd.read_csv(input1)
     dfCO = pd.read_csv(input2)
 
